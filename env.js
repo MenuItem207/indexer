9bacf806b11737deedb716f4bc03ca2ba5662d0f
Emmanuel <70802541+MenuItem207@users.noreply.github.com>
2022-08-10 19:19:48 +0800

    Updated documentation

--- env.js
+++ env.js
@@ -6,6 +6,8 @@ const PATH_TO_INDEXJS = '.././src/index.js';
 /**
  * legacy window.translations object
  * TODO: copy-paste the legacy object here if you want to convert into a json file
+ * i.e
+ * legacy_obj = {};
  */
 const legacy_obj = undefined;
 
